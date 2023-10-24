f1824c30d3073eff4ad51a4e4fb2642ab1dd586f
mahdi shafiei <mahdishafiei74@gmail.com>
2023-10-24 17:31:20 +0330

    3.py added

--- Challenges/projectEuler/3.py
+++ Challenges/projectEuler/3.py
@@ -1,12 +1,22 @@
 import eulerlib
 
+def compute():
+	n = 600851475143
+	while True:
+		p = smallest_prime_factor(n)
+		if p < n:
+			n //= p
+		else:
+			return str(n)
 
 
-a = 600851475143
+def smallest_prime_factor(n):
+	assert n >= 2
+	for i in range(2, eulerlib.sqrt(n) + 1):
+		if n % i == 0:
+			return i
+	return n  # n itself is prime
 
 
-
-
-compute():
-    while x <= a:
-        if x % a & x % 1 == 0:
+if __name__ == "__main__":
+	print(compute())
